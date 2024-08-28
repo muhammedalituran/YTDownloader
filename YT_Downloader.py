f96d0bd70fdf8cd2c2013384ed3b9034abb0c368
Muhammed Ali Turan <m_ali_turan@outlook.com>
2024-08-28 09:43:19 +0300

    V0.8 video download feature added

--- YT_Downloader.py
+++ YT_Downloader.py
@@ -11,8 +11,7 @@ import os
 
 
 
-downloads_path = str(Path.home() / "Downloads")
-download_folder = downloads_path + "\YoutubeDownloads_byTuran"
+
 
 
 yt=""
@@ -21,40 +20,44 @@ def youtube_object_creater(url=""):
     global yt
     yt = YouTube(url)
 
-def get_resolutions(stream_query):
+def get_resolutions():
+    stream_query = yt.streams.filter(file_extension="mp4",type="video").order_by("resolution")
     resolutions = []
     for res in stream_query:
         if (res.resolution not in resolutions):
             resolutions.append(res.resolution)
     return resolutions
 
-def downloader(res):
+def downloader(res,downloads_path,download_folder):
     if not os.path.exists(download_folder):
         os.makedirs(download_folder)
     video = yt.streams.filter(res=res).first()
     if video.is_progressive == True:
-        video.download()
+        video.download(filename=f"{download_folder}\\{yt.title}_{res}.mp4")
     else:
         video.download(filename=f"{download_folder}\\video1.mp4")
         yt.streams.filter(type="audio").order_by("abr").desc().first().download(filename=f"{download_folder}\\audio1.mp3")
-        video_combiner(res)
+        video_combiner(res,downloads_path,download_folder)
 def get_thumbnail():
     thumbnail = yt.thumbnail_url
     return thumbnail
 
-        
+def get_title():
+    title = yt.title
+    return title
 def audio_download():
+    downloads_path = str(Path.home() / "Downloads")
+    download_folder = downloads_path + "\YoutubeDownloads_byTuran"
     yt.streams.filter(type="audio").order_by("abr").desc().first().download(filename=f"{download_folder}\\{yt.title}.mp3")
 
     
     
-def video_combiner(res):
+def video_combiner(res,downloads_path,download_folder):
 
     my_clip = mpe.VideoFileClip(f"{download_folder}\\video1.mp4")
     audio_background = mpe.AudioFileClip(f"{download_folder}\\audio1.mp3")
     final_clip = my_clip.set_audio(audio_background)
-    video_name = input("Please Enter A Video Title After Saving:")
-    final_clip.write_videofile(f"{download_folder}\\{video_name}.mp4", codec="libx264")
+    final_clip.write_videofile(f"{download_folder}\\{yt.title}_{res}.mp4", codec="libx264")
     os.remove(f"{download_folder}\\video1.mp4")
     os.remove(f"{download_folder}\\audio1.mp3")
     print("Download Completed!")
--- Yt_Downloader_Gui.py
+++ Yt_Downloader_Gui.py
@@ -1,41 +1,90 @@
 from tkinter import *
+from tkinter import ttk,messagebox
 import YT_Downloader
 from PIL import Image,ImageTk
 from urllib.request import urlopen
-
-
+from pathlib import Path
+import os
+import moviepy.editor as mpe
 
 
 #variables
-
+res_list = []
 
 
 
 window = Tk()
-window.minsize(width=600,height=450)
+window.minsize(width=850,height=450)
 window.title("Youtube Downloader \ ByTuran")
 
 
-
+#Enter url and chech link
 link_label = Label(text="Enter URL:")
-link_label.grid(row=0,column=0)
+link_label.place(x=0,y=10)
 
-link_entry = Entry()
-link_entry.grid(row=0,column=1)
+link_entry = Entry(width=60)
+link_entry.place(x=60,y=10)
 
 link_button = Button(text="Check",command=lambda:[YT_Downloader.youtube_object_creater(link_entry.get()),get_thumbnail_gui()])
-link_button.grid(row=0,column=3)
+link_button.place(x=430,y=7)
 
+
+#Get video thumbnail and title
 thumbnail_label = Label(image="")
-thumbnail_label.grid(row=1,column=4)
+thumbnail_label.place(x=480,y=25)
+
+title_lable = Label()
+title_lable.place(x=480,y=170)
+
+#Select download video or audio
+
+def radiocheck_selected():
+    return radio_check_state.get()
+
+
+res_combobox = ttk.Combobox()
+
+def video_download_menu():
+    res_list = YT_Downloader.get_resolutions()
+    res_combobox.config(values=res_list)
+    res_combobox.set("Pick a resolution")
+    res_combobox.place(x=10,y=80)
+    
+
+
+
+
+
+radio_check_state = IntVar()
+emty_radiobutton = Radiobutton(text="bos",value=0,variable= radio_check_state,command=video_download_menu)
+video_radiobutton = Radiobutton(text="Download Video",value=1,variable= radio_check_state,command=video_download_menu)
+video_radiobutton.place(x=10,y=50)
+
+audio_radiobutton = Radiobutton(text="Download Audio",value=2,variable= radio_check_state,command=radiocheck_selected)
+audio_radiobutton.place(x=130,y=50)
+
+
+def download():
+    downloads_path = str(Path.home() / "Downloads")
+    download_folder = downloads_path + "\YoutubeDownloads_byTuran"
+    YT_Downloader.downloader(res_combobox.get(),downloads_path,download_folder)
+    messagebox.showinfo("Download Info","Download Complited")
+
+
+
+download_button = Button(text="Download",command=download)
+download_button.place(x=300,y=100)
+
 
 def get_thumbnail_gui():
     
     th_url = YT_Downloader.get_thumbnail()
-    img = Image.open(urlopen(th_url)).resize((200,200), Image.LANCZOS)
+    img = Image.open(urlopen(th_url)).resize((256,144), Image.LANCZOS)
     ph = ImageTk.PhotoImage(img)
     thumbnail_label.config(image=ph)
     thumbnail_label.image = ph
+    video_title = YT_Downloader.get_title()
+    title_lable.config(text=video_title)
     
 
 window.mainloop()
